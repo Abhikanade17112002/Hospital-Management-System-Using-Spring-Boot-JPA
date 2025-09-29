166b6822ef8f6f9e5722e3f0822c967419e1b3dc
Abhishek Rangnath Kanade <abhishek.kanade@encora.com>
2025-09-29 19:30:59 +0530

    initial commit

---
+++ src/main/java/com/jpa/AnujJpaApplication.java
@@ -0,0 +1,13 @@
+package com.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnujJpaApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(AnujJpaApplication.class, args);
+	}
+
+}
